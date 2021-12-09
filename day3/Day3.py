3b7455c67d111cb6cbdfcf1450a19ac992bb5bb3
SCo <samuel.cordano@ekimetrics.com>
2021-12-09 14:21:59 +0100

    DAY3 part 1

---
+++ day3/Day3.py
@@ -0,0 +1,34 @@
+if __name__ == "__main__":
+    with open("day3/inputTest.txt") as input:
+        inputLines = [str(line.strip()) for line in input]
+
+    input_list = []
+    for line in inputLines:
+        input_line = []
+        currentInput = line.strip()
+        for character in currentInput:
+            input_line.append(character)
+        input_list.append(input_line)
+
+    gammaRateBinary = []
+    epsilonRateBinary = []
+    for index_column in range(len(input_list[0])):
+        countOfZeroes = 0
+        countOfOnes = 0
+        for index_row in range(len(input_list[0])):
+            if input_list[index_row][index_column] == "0":
+                countOfZeroes += 1
+            else:
+                countOfOnes += 1
+        # Once all rows have been counted
+        if countOfZeroes > countOfOnes:
+            gammaRateBinary.append("0")
+            epsilonRateBinary.append("1")
+        else:
+            gammaRateBinary.append("1")
+            epsilonRateBinary.append("0")
+
+    gammaRateBinary = int("".join(gammaRateBinary), 2)
+    epsilonRateBinary = int("".join(epsilonRateBinary), 2)
+    result = gammaRateBinary * epsilonRateBinary
+    print(f"Q1 result: {result}")
